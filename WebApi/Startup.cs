1bac247208f6af0b30353bd3c84d95a59be8b0c5
Figueus <j-c-s-s@hotmail.com>
2017-11-07 18:51:10 +0100

    Any header + any method

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -114,7 +114,10 @@ namespace WebApi
             }
 
             app.UseCors(builder =>
-            builder.WithOrigins("https://localhost:44314"));
+            builder.WithOrigins("https://localhost:44314")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            );
 
             app.UseSwagger();
             app.UseSwaggerUI(c => 
