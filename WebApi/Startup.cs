a8ca9ff04299050074289441bf2895f801b7dc9e
Bas <b.d.r._bas@live.nl>
2017-11-10 11:28:43 +0100

    users can create an appointment

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -115,7 +115,8 @@ namespace WebApi
 
             app.UseCors(builder =>
             builder.WithOrigins("https://localhost:44314").
-            AllowAnyHeader().AllowAnyMethod());
+            AllowAnyHeader()
+            .AllowAnyMethod());
 
             app.UseSwagger();
             app.UseSwaggerUI(c => 
--- WebApplication/wwwroot/app/controllers/createAppController.js
+++ WebApplication/wwwroot/app/controllers/createAppController.js
@@ -16,11 +16,15 @@
 
         $scope.sendDataObject = {}
         
-        $scope.sendDataObject.date = $scope.appointment.date;
-        $scope.sendDataObject.time = $scope.appointment.time;
+        $date = $scope.appointment.date;
+        $time = $scope.appointment.time;
+        $dateTime = $date + "T" + $time + ":00.00Z";
+
+        $scope.sendDataObject.time = $dateTime;
         $scope.sendDataObject.benchId = $scope.appointment.bench.id;
         $scope.sendDataObject.clientId = $scope.appointment.clientId;
-        $scope.sendDataObject.healthworkerName = $scope.appointment.healthworkerName;
+        $scope.sendDataObject.healthworkerId = "0bba7a60-d5ff-4e53-bdf9-e00bc7552f10";
+        console.log($scope.sendDataObject);
 
         $http.post('http://127.0.0.1:54618/api/Appointments/', $scope.sendDataObject)
             .then(function (response) {
@@ -30,6 +34,7 @@
             }, function (response) {
                 //second function handles error
                 alert('something went wrong!');
+                console.log(response);
 
             });
     };
