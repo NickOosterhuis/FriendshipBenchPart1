7f5efeacaebd8a2a0fbfabe1deddf595afddb07d
Nick Oosterhuis <nickoosterhuis@gmail.com>
2017-11-12 23:15:54 +0100

    Get users and edit users (bug password cant be changed atm)

--- MobileApp/MobileApp/ViewModels/EditUserViewModel.cs
+++ MobileApp/MobileApp/ViewModels/EditUserViewModel.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
-namespace WebApi.Models
+namespace MobileApp.ViewModels
 {
-    public class ClientUser : User
+    class EditUserViewModel
     {
-        public string StreetName { get; set; }
+        public string Email { get; set; }
 
+        public string Password { get; set; }
+
+        public string StreetName { get; set; }
+        
         public string HouseNumber { get; set; }
 
         public string Province { get; set; }
 
         public string District { get; set; }
-
-        public string HealthWorker_Id { get; set; }
     }
 }
