39e05282200652944c48082c3dfd635b0a020e63
lightnessking <moostebring@hotmail.com>
2017-11-13 17:06:40 +0100

    changes

--- WebApplication/Controllers/AccountController.cs
+++ WebApplication/Controllers/AccountController.cs
@@ -93,38 +93,6 @@ namespace WebApplication.Controllers
                 Debug.WriteLine(response.Headers);
                 return View(model);
             }
-            /*
-            ViewData["ReturnUrl"] = returnUrl;
-            if (ModelState.IsValid)
-            {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
-                if (result.Succeeded)
-                {
-                    _logger.LogInformation("User logged in.");
-                    return RedirectToLocal(returnUrl);
-                }
-                if (result.RequiresTwoFactor)
-                {
-                    return RedirectToAction(nameof(LoginWith2fa), new { returnUrl, model.RememberMe });
-                }
-                if (result.IsLockedOut)
-                {
-                    _logger.LogWarning("User account locked out.");
-                    return RedirectToAction(nameof(Lockout));
-                }
-                else
-                {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-                    return View(model);
-                }
-            }
-
-    */
-
-            // If we got this far, something failed, redisplay form
-            return View(model);
         }
 
         [HttpGet]
---
+++ WebApplication/wwwroot/app/controllers/appointmentsController.js
@@ -0,0 +1,17 @@
+﻿var appointments = angular.module('appointments', []);
+
+appointments.controller('appointmentscontroller', function ($scope, $http) {
+    var cookie = getCookie("JWT");
+    $http.get('http://127.0.0.1:54618/api/appointments/', {
+        headers: {
+            'Authorization': "Bearer " + getCookie("JWT")
+        }
+    })
+    .then(function (response) {
+        $scope.appointments = response.data;
+        console.log("succes");
+    }, function (response) {
+        $scope.appointments = "something went wrong! Please contact an administrator.";
+        console.log("failed");
+    });
+});
