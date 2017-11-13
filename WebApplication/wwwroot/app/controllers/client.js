898daeb2fb99fcd8c3959c4e5998824eb2f5c05d
Figueus <j-c-s-s@hotmail.com>
2017-11-13 18:00:33 +0100

    vergeten

---
+++ WebApplication/wwwroot/app/controllers/client.js
@@ -0,0 +1,45 @@
+﻿//module
+var appClient = angular.module('clients', ['ngRoute']);
+
+appClient.config(['$locationProvider', function ($locationProvider) {
+    $locationProvider.html5Mode(true);
+    $locationProvider.hashPrefix('');
+}]);
+
+//controller
+appClient.controller('clientsCtrl', function ($scope, $http, $location) {
+    var cookie = getCookie("JWT");
+    var email = getCookie("email");
+    //get all clients connected to the logged in healthworker
+    $http.get('http://127.0.0.1:54618/api/Clients/connected/' + email, {
+        headers: {
+            'Authorization': "Bearer " + cookie
+        }
+    })
+        .then(function (response) {
+            //first function handles succes
+            $scope.clients = response.data;
+            $location.path("/clients");
+
+        }, function (response) {
+            //second function handles error
+            alert('error');
+            $scope.clients = "something went wrong!";
+
+        });
+
+    // callback for ng-click 'showClient':
+    $scope.showClient = function (ClientID) {
+        console.log('redirecting to: ' + "/clients/show/" + ClientID);
+        $location.path("/clients/show/" + ClientID);
+    };
+
+ });
+
+appClient.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/clients', { templateUrl: '/app/views/clients/list.html', controller: 'clientsCtrl' });
+    $routeProvider.when('/clients/show/:id', { templateUrl: '/app/views/clients/show.html', controller: 'showClientCtrl' });
+    $routeProvider.otherwise({ redirectTo: '/' });
+}]);
+
+
